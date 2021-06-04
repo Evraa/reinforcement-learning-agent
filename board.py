9bfdd47ad3f8a3a1c7d990c6c15ff67f054b54f7
Evraa <evramyousef@gmail.com>
2021-06-04 20:11:28 +0200

    added: policy positive and negative

--- board.py
+++ board.py
@@ -4,39 +4,82 @@ class StateBoard:
     """
     Stating board info
     """
-    def __init__(self, N,M,val=0):
+    def __init__(self, N, M, agent_i=0, agent_j=0, void_cost=-0.04, goal_cost=5, exit_cost=-5, state=0):
         self.N = N
         self.M = M
+        self.state_value = {}
+        self.state_value[0] = void_cost
+        self.state_value[1] = goal_cost
+        self.state_value[-1] = exit_cost
+        self.state_value[2] = 0
+        self.agent = [agent_i, agent_j]
+
         self.board = []
         for _ in range (N):
             row = []
-            for _ in range (M): row.append(0)
+            for _ in range (M): row.append(state)
             self.board.append(row)
 
-    def assign_goal(self, i, j, val=1):
+        
+
+    def assign_goal(self, i, j, state=1):
         assert i<self.N and j<self.M, "Out of bound assignment!"
         try:
-            self.board[i][j] = val
+            self.board[i][j] = state
         except:
             print ("Error: cant assign goal state")
     
-    def assign_exit(self, i, j, val=-1):
+    def assign_exit(self, i, j, state=-1):
         assert i<self.N and j<self.M, "Out of bound assignment!"
         try:
-            self.board[i][j] = val
+            self.board[i][j] = state
         except:
             print ("Error: cant assign exit state")
 
-    def assign_obstacle(self, i, j, val=2):
+    def assign_obstacle(self, i, j, state=2):
         assert i<self.N and j<self.M, "Out of bound assignment!"
         try:
-            self.board[i][j] = val
+            self.board[i][j] = state
         except:
             print ("Error: cant assign obstacle state")
         
     
     def print_board(self):
         print (self.board)
-    
+        print ("State vlaues:")
+        temp_state = []
+        for i in range(self.N):
+            temp_row = []
+            for j in range (self.M): temp_row.append(self.state_value[self.board[i][j]])
+            temp_state.append(temp_row)
+        print (temp_state)
+
+
     def get_dim(self):
         return self.N, self.M
+
+    def check_invalid_index(self, coord):
+        if coord[0] <0 or coord[1]<0 or coord[0] >= self.N or coord[1]>=self.M:
+            return True
+        if self.board[coord[0]][coord[1]] == 2:
+            return True
+        return False
+
+    def get_goals(self):
+        goals = []
+        for i in range(self.N):
+            for j in range(self.M):
+                if self.board[i][j] == 1:
+                    goal = [i,j]
+                    goals.append(goal)
+        return goals
+
+    def get_exits(self):
+        exits = []
+        for i in range(self.N):
+            for j in range(self.M):
+                if self.board[i][j] == -1:
+                    exit = [i,j]
+                    exits.append(exit)
+        return exits
+
---
+++ policy.py
@@ -0,0 +1,73 @@
+import numpy as np
+import sys
+import random
+
+def get_avg(listy):
+    listy_np = np.array(listy)
+    return listy_np.mean(axis=0)
+
+
+
+def direction(i,j,x,y):
+    tot = abs(x-i) + abs(y-j)
+    rand = random.random() #[0,1)
+    if tot == 0:
+        # same point
+        return 'same'
+    if abs(x-i) > abs(y-j):
+        if abs(y-j)/tot >= rand:
+            # surprisingly favourite y
+            if y>j: return "r"
+            else: return "l"
+        else:
+            # favourite x
+            if x>i: return "b"
+            else: return "t"
+    else:
+        if abs(x-i)/tot >= rand:
+            # surprisingly favourite x
+            if x>i: return "b"
+            else: return "t"
+        else:
+            # favourite y
+            if y>j: return "r"
+            else: return "l"
+
+
+def invert_policy(symbol):
+    if symbol == "r":return "l"
+    if symbol == "l":return "r"
+    if symbol == "t":return "b"
+    if symbol == "b":return "t"
+    if symbol == "same":return "same"
+    
+    
+
+def get_policy(board, policy="goal"):
+    # find all goals i,j
+    N,M = board.get_dim()
+    # get goals
+    if policy=="goal":
+        goals = board.get_goals()
+    elif policy=="exit":
+        goals = board.get_exits()
+
+    if len(goals) == 0: 
+        print ("Error: can't find any goal/exit")
+        sys.exit(1)
+    
+    avg_point = get_avg(goals)
+    policy_board = []
+    for i in range (N):
+        policy_row = []
+        for j in range (M):
+            d = direction(i,j,avg_point[0],avg_point[1])
+            if policy =="exit": 
+                d = invert_policy(d)
+            print (i,j)
+            print (d)
+            policy_row.append(d)
+        policy_board.append(policy_row)
+
+    return policy_board
+
